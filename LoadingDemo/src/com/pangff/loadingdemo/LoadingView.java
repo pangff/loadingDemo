19326722a440c2c6fcaac6ce1a09ddea0016ebd3
pangff <pangff@gmail.com>
2013-11-27 11:26:12 +0800

    添加全局loading支持

--- LoadingDemo/src/com/pangff/loadingdemo/LoadingView.java
+++ LoadingDemo/src/com/pangff/loadingdemo/LoadingView.java
@@ -7,25 +7,42 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-public class LoadingView extends RelativeLayout{
+public class LoadingView extends RelativeLayout {
 	private View view;
+
 	public LoadingView(Context context) {
 		super(context);
-		view = ((Activity)context).getLayoutInflater().inflate(R.layout.loading, null);
+		view = ((Activity) context).getLayoutInflater().inflate(
+				R.layout.loading, null);
 	}
-	
-	public void addLoadingTo(ViewGroup anchor){
-		ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-		anchor.addView(view, lp);
+
+	public void addLoadingTo(ViewGroup anchor) {
+		ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		if (anchor == null) {
+			ViewGroup root = (ViewGroup) ((ViewGroup) ((Activity) getContext())
+					.findViewById(android.R.id.content)).getChildAt(0);
+			root.addView(view, lp);
+		} else {
+			anchor.addView(view, lp);
+		}
 	}
-	
-	public void removeLoadingFrom(ViewGroup anchor){
-		anchor.removeView(view);
+
+	public void removeLoadingFrom(ViewGroup anchor) {
+		if (anchor == null) {
+			ViewGroup root = (ViewGroup) ((ViewGroup) ((Activity) getContext())
+					.findViewById(android.R.id.content)).getChildAt(0);
+			root.addView(view);
+		} else {
+			anchor.removeView(view);
+		}
+
 	}
 
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
 		return true;
 	}
-	
+
 }
